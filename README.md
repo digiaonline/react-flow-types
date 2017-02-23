a45ff1f5e2e85dbb0916e0b1158ac652a7eb89a4
AriaMinaei <aria.minaei@gmail.com>
2017-02-23 17:19:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,169 @@
+# react-types
+
+A small collection of flow type definitions for working with React components.
+
+## Usage
+
+```
+$ npm install --save-dev @nordsoftware/react-types
+```
+
+```js
+import type {ReactChildren} from '@nordsoftware/react-types'
+
+export default function MyComponent({children}: {children?: ReactChildren}) =>
+  <div>{chlidren}</div>
+```
+
+## The types
+
+### `Element<Config>`
+
+Matches a valid instance of a react component, also called an element. It's a alias for the global type `React$Element`.
+
+Example:
+
+```
+import type {Element} from '@nordsoftware/react-types'
+
+export default function Layout({header, footer}: {header: Element<any>, footer: Element<any>}) =>
+  <div>
+    <div class="header">{header}</div>
+    <div class="footer">{footer}</div>
+  </div>
+```
+
+### `AnyReactElement`
+
+Useful a value that is an `Element<any>` or `void`. Useful when a value should be an optional single element.
+
+Example:
+
+```
+import type {AnyReactElement} from '@nordsoftware/react-types'
+
+// `header` and `footer` in this example are optional
+export default function Layout({header, footer}: {header: AnyReactElement, footer: AnyReactElement}) =>
+  <div>
+    <div class="header">{header}</div>
+    <div class="footer">{footer}</div>
+  </div>
+```
+
+### `AnyReactElement`
+
+Matches a single `AnyReactElement`, or an arary of them, or a string, or a number. Basically, any value that can be the `children` of a `<div>` will flow into this.
+
+Example:
+
+```
+import type {ReactChildren} from '@nordsoftware/react-types'
+
+// `header` and `footer` in this example can be multiple elements, and even strings or numbers
+export default function Layout({header, footer}: {header: ReactChildren, footer: ReactChildren}) =>
+  <div>
+    <div class="header">{header}</div>
+    <div class="footer">{footer}</div>
+  </div>
+```
+
+## `FunctionalComponent<Props>`
+
+Matches a functional react component.
+
+Example:
+
+```
+import type {FunctionalComponent} from '@nordsoftware/react-types'
+
+const TheComponent = () => <div />
+
+const makeElement = (component: FunctionalComponent<{n: number}>) =>
+  React.createElement(component, {n: 10})
+
+makeElement(TheComponent)
+```
+
+## `ClassComponent<DefaultProps, Props, State>`
+
+An alias for `Class<React$Component<DefaultProps, Props, State>>`, and matches any ES6 class-based react componnet.
+
+Example:
+
+```
+import type {ClassComponent} from '@nordsoftware/react-types'
+
+class TheComponent extends React.Component {
+  props: {n: number}
+  render() {
+    return <div />
+  }
+}
+
+const makeElement = (component: ClassComponent<any, {n: number}, any>) =>
+  React.createElement(component, {n: 10})
+
+makeElement(TheComponent)
+```
+
+## `Component<Props>`
+
+Matches any component, whether functional or class-based.
+
+```
+import type {Component} from '@nordsoftware/react-types'
+
+class TheComponent extends React.Component {
+  props: {n: number}
+  render() {
+    return <div />
+  }
+}
+
+const AnotherComponent = () => <div />
+
+const makeElement = (component: Component<{n: number}>) =>
+  React.createElement(component, {n: 10})
+
+makeElement(TheComponent)
+makeElement(AnotherComponent)
+```
+
+## `HOC<RequiredProps, ProvidedProps>`
+
+The generic type of a higher-order component. A `HOC` always *provides* a set of props to the inner component, and *requires* another set of props to be passed to it.
+
+Example:
+
+```
+import type {HOC} from '@nordsoftware/react-types'
+
+type RequiredProps = {
+  name: string,
+}
+
+type ProvidedProps = {
+  input: {
+    value: mixed,
+    onChange: Function,
+  },
+}
+
+// The hoc:
+const asField = (): HOC<{name: string}, ProvidedProps> => (component): any => {
+  const FinalComponent = ({name, ...rest}) =>
+    <ReduxFormField name={name} component={component} props={rest} />;
+
+  hoistNonReactStatics(FinalComponent, component)
+
+  FinalComponent.displayName =
+    `asField(${component.displayName || component.name || 'Component'})`
+
+  return FinalComponent
+}
+
+const Input = ({input}) => <input type="text" {...input} />
+const WrapperInput = asField(Input)
+
+const element = <WrappedInput name="email" />
+```
---
+++ tests/index.js
@@ -0,0 +1,163 @@
+// @flow
+import React from 'react';
+import type {
+  FunctionalComponent,
+  ClassComponent,
+  HOC,
+  Component,
+  Element,
+  AnyReactElement,
+  ReactChildren,
+} from '../index';
+
+// This is a valid functional react component that we'll use to test our HOCs later
+const ValidFunctionalComponent = (props: {string1: string, number1: number}) => <div />;
+
+// This is an invalid functional component. HOCs shouldn't accept this as input
+const InvalidFunctionalComponent = (props: {string1: string, number1: number}) => 'hi';
+
+// Let's test them both:
+(function(){
+  // This should pass:
+  <ValidFunctionalComponent string1="string" number1={10} />;
+
+  // $FlowExpectError
+  <ValidFunctionalComponent string1="string" number1="string" />;
+
+  // $FlowExpectError
+  <InvalidFunctionalComponent string1="string" number1={10} />;
+})();
+
+// Now we can test FunctionalComponent:
+(function(){
+  // If FunctionalComponent is correctly defined, then this should pass:
+  (function<Props>(a: FunctionalComponent<Props>){})(ValidFunctionalComponent);
+
+  // ... and so should this:
+  (function(a: FunctionalComponent<{string1: string, number1: number}>){})(ValidFunctionalComponent);
+
+  // ... but this should fail: (commenting out, because error shows up in the wrong locaiton. That's a bug with flow)
+  // (function(a: FunctionalComponent<any>){})(InvalidFunctionalComponent);
+})();
+
+// This is a valid class-based component. We'll use it to test HOCs later
+class ValidClassComponent extends React.Component<{number1: number}, {string1: string, number1: number}, void> {
+  static defaultProps = {number1: 10}
+
+  render() {
+    return <div />;
+  }
+};
+
+// Let's test it:
+(function(){
+  // This should pass:
+  <ValidClassComponent string1="string" />;
+
+  // $FlowExpectError
+  <ValidClassComponent strig1={10} />;
+})();
+
+// Now we can test ClassComponent
+(function(){
+  // This should pass:
+  (function(a: ClassComponent<any, {string1: string, number1: number}, any>){})(ValidClassComponent);
+
+  // $FlowExpectError
+  (function(a: ClassComponent<any, {string1: string, number1: string}, any>){})(ValidClassComponent);
+
+  // $FlowExpectError
+  (function(a: ClassComponent<any, {string1: string, number1: string}, any>){})(ValidFunctionalComponent);
+})();
+
+// Tests for Component:
+(function(){
+  // Should pass:
+  (function(a: Component<{string1: string, number1: number}>){})(ValidClassComponent);
+  (function(a: Component<{string1: string, number1: number}>){})(ValidFunctionalComponent);
+
+  // Should fail: $FlowExpectError
+  (function(a: Component<{string1: string}>){})(ValidFunctionalComponent);
+
+  // $FlowExpectError
+  (function(a: Component<{string1: string}>){})(ValidClassComponent);
+})();
+
+// Tests for Element:
+(function(){
+  (<div />: Element<any>);
+  (<ValidFunctionalComponent string1="string" number1={10} />: Element<any>);
+})();
+
+// Tests for AnyReactElement
+(function(){
+  ((<div />): AnyReactElement);
+  ((null): AnyReactElement);
+  // $FlowExpectError
+  (('string'): AnyReactElement);
+})();
+
+// Tests for ReactChildren
+(function(){
+  ((<div />): ReactChildren);
+  (([<div />]): ReactChildren);
+
+  ((null): ReactChildren);
+  (([null]): ReactChildren);
+
+  (('string'): ReactChildren);
+  // $FlowExpectError
+  ((['string']): ReactChildren);
+
+  ((10): ReactChildren);
+
+  // $FlowExpectError
+  (({}): ReactChildren);
+})();
+
+// Now, the tests for HOC:
+(function() {
+  // Let's start with only the ProvidedProps part:
+  declare var provideString1: HOC<{}, {string1: string}>;
+
+  (function(){
+    // ... and test it with our functional component:
+    const ProvideString1OfValidFunctionalComponent = provideString1(ValidFunctionalComponent);
+
+    // should pass:
+    (<ProvideString1OfValidFunctionalComponent number1={10} />: Element<any>);
+  })();
+
+  (function(){
+    // Now with the class-based component:
+    const ProvideString1OfValidClassComponent = provideString1(ValidClassComponent);
+
+    // should pass:
+    (<ProvideString1OfValidClassComponent />: Element<any>);
+  })();
+
+  // now both ProvidedProps and RequiredProps:
+  declare var provideString1AndRequireObject1: HOC<{object1: Object}, {string1: string}>;
+
+  (function(){
+    // with the functional component:
+    const ProvideString1AndRequireObject1OfValidFunctionalComponent = provideString1AndRequireObject1(ValidFunctionalComponent);
+
+    // should pass:
+    <ProvideString1AndRequireObject1OfValidFunctionalComponent object1={{}} number1={10} />;
+
+    // $FlowExpectError
+    <ProvideString1AndRequireObject1OfValidFunctionalComponent number1={10} />;
+  })();
+
+  (function(){
+    // with the class component:
+    const ProvideString1AndRequireObject1OfValidClassComponent = provideString1AndRequireObject1(ValidClassComponent);
+
+    // should pass:
+    <ProvideString1AndRequireObject1OfValidClassComponent object1={{}} />;
+
+    // $FlowExpectError
+    <ProvideString1AndRequireObject1OfValidClassComponent number1={10} />;
+  })();
+})();
