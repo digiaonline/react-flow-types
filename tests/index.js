e921c9eab9ee4ff2af3649b221f020b492f5c483
AriaMinaei <aria.minaei@gmail.com>
2017-02-25 15:52:55 +0100

    Add tests for usage with ramda/compose

--- tests/index.js
+++ tests/index.js
@@ -174,5 +174,35 @@ class ValidClassComponent extends React.Component<{number1: number}, {string1: s
     <ComposedComponent number1={1} number3={1} />;
     // $FlowExpectError
     <ComposedComponent number1={1} number2={1} />;
+
+    // using ramda's compose:
+    type FN<A,R> = (a: A) => R;
+    declare var compose:
+      ((end: void) => (<T>(x: T) => T)) &
+      (<A,B>(m1: FN<A,B>, end: void) => FN<A,B>) &
+      (<A,B,C>(m1: FN<B,C>, m2: FN<A,B>, end: void) => FN<A,C>) &
+      (<A,B,C,D>(m1: FN<C,D>, m2: FN<B,C>, m3: FN<A,B>, end: void) => FN<A,D>) &
+      (<A,B,C,D,E>(m1: FN<D,E>, m2: FN<C,D>, m3: FN<B,C>, m4: FN<A,B>, end: void) => FN<A,E>) &
+      (<A,B,C,D,E,F>(m1: FN<E,F>, m2: FN<D,E>, m3: FN<C,D>, m4: FN<B,C>, m5: FN<A,B>, end: void) => FN<A,F>) &
+      (<A,B,C,D,E,F,G>(m1: FN<F,G>, m2: FN<E,F>, m3: FN<D,E>, m4: FN<C,D>, m5: FN<B,C>, m6: FN<A,B>, end: void) => FN<A,G>) &
+      (<A,B,C,D,E,F,G,H>(m1: FN<G,H>, m2: FN<F,G>, m3: FN<E,F>, m4: FN<D,E>, m5: FN<C,D>, m6: FN<B,C>, m7: FN<A,B>, end: void) => FN<A,H>) &
+      (<A,B,C,D,E,F,G,H,J>(m1: FN<G,J>, m2: FN<G,H>, m3: FN<F,G>, m4: FN<E,F>, m5: FN<D,E>, m6: FN<C,D>, m7: FN<B,C>, m8: FN<A,B>, end: void) => FN<A,J>) &
+      (<A,B,C,D,E,F,G,H,J,K>(m1: FN<J,K>, m2: FN<G,J>, m3: FN<G,H>, m4: FN<F,G>, m5: FN<E,F>, m6: FN<D,E>, m7: FN<C,D>, m8: FN<B,C>, m9: FN<A,B>, end: void) => FN<A,K>) &
+      (<A,B,C,D,E,F,G,H,J,K,L>(m1: FN<K,L>, m2: FN<J,K>, m3: FN<G,J>, m4: FN<G,H>, m5: FN<F,G>, m6: FN<E,F>, m7: FN<D,E>, m8: FN<C,D>, m9: FN<B,C>, m10: FN<A,B>, end: void) => FN<A,L>) &
+      (<A,R>(...funcs: Array<FN<A,R>>) => FN<A,R>);
+
+      const ComposedComponent2 = compose(
+        requireNumber3,
+        provideNumber1AndRequireNumber2,
+        provideString1,
+      )(ValidFunctionalComponent);
+
+      <ComposedComponent2 number1={1} number2={1} number3={1} />;
+      // $FlowExpectError
+      <ComposedComponent2 number2={1} number3={1} />;
+      // $FlowExpectError
+      <ComposedComponent2 number1={1} number3={1} />;
+      // $FlowExpectError
+      <ComposedComponent2 number1={1} number2={1} />;
   })();
 })();
